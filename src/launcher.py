ae1912fc58caddb91c79d98ffc72a7d1f84fe557
Hiromitsu Yamaguchi <yamagame@mac.com>
2025-05-04 11:35:47 +0900

    起動スクリプト修正

--- src/launcher.py
+++
@@ -1,6 +0,0 @@
-# title: Copilot Shooter
-# author: yamagame
-# desc: Sample pyxel shooting game with Gihub Copilot
-# site: https://github.com/yamagame/copilot-shooter
-# license: MIT
-# version: 1.0
--- src/main.py
+++ src/main.py
@@ -1,3 +1,9 @@
+# title: Copilot Shooter
+# author: yamagame
+# desc: Sample pyxel shooting game with Gihub Copilot
+# site: https://github.com/yamagame/copilot-shooter
+# license: MIT
+# version: 1.0
 import pyxel
 from obj.player import Player
 from obj.enemy import Enemy
